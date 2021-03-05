3a3959d30e7f4e38f62d1d01947de37507960310
Leonard Ehrenfried <mail@leonard.io>
2021-03-05 11:05:46 +0100

    Connect to Thingsboard

--- gtfsrthttp2mqtt.py
+++ gtfsrthttp2mqtt.py
@@ -11,6 +11,41 @@ from requests.packages.urllib3.util.retry import Retry
 import gtfs_realtime_pb2
 import utils
 
+import glob
+import requests
+import os
+import datetime
+import json
+from argparse import ArgumentParser
+import time
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from functools import lru_cache
+
+class ThingsboardClient:
+    def __init__(self):
+        self.base_url = os.environ['THINGSBOARD_HOST']
+
+    def get_token(self):
+        token_url = f"{self.base_url}/auth/login"
+
+        payload = {
+            "username": os.environ['THINGSBOARD_USERNAME'],
+            "password": os.environ['THINGSBOARD_PASSWORD']
+        }
+
+        response = requests.post(token_url, json=payload)
+        return response.json()["token"]
+
+    def fetch_timeseries(id, token):
+        auth_headers = {
+            "X-Authorization": f"Bearer {token}"
+        }
+        timeseries_url = f"{self.base_url}/plugins/telemetry/DEVICE/{id}/values/timeseries"
+        return requests.get(timeseries_url, headers=auth_headers).json()
+
+    def get_vehicles(self):
+        token = self.get_token()
+        return [{ "latitude": 48, "longitude": 8, "id": "vehicle-1" }]
 
 ## https://stackoverflow.com/questions/22498038/improve-current-implementation-of-a-setinterval-python/22498708#22498708
 def call_repeatedly(interval, func, *args):
@@ -29,7 +64,6 @@ class GTFSRTHTTP2MQTTTransformer:
     def __init__(self, mqttConnect, mqttCredentials, baseMqttTopic):
         self.mqttConnect = mqttConnect
         self.mqttCredentials = mqttCredentials
-        print(self.mqttCredentials)
         self.baseMqttTopic = baseMqttTopic
         self.mqttConnected = False
         self.session = requests.Session()
@@ -50,10 +84,8 @@ class GTFSRTHTTP2MQTTTransformer:
         self.client = mqtt.Client()
         self.client.on_connect = self.onMQTTConnected
         self.client.tls_set(cert_reqs=ssl.CERT_REQUIRED, tls_version=ssl.PROTOCOL_TLS)
+        self.client.username_pw_set(**self.mqttCredentials)
         self.client.connect(**self.mqttConnect)
-        if self.mqttCredentials and self.mqttCredentials['username'] and self.mqttCredentials['password']:
-            print("settttttinnnggg")
-            self.client.username_pw_set(**self.mqttCredentials)
         self.client.loop_forever()
 
     def startThingsboardPolling(self):
@@ -63,12 +95,9 @@ class GTFSRTHTTP2MQTTTransformer:
 
     def doThingsboardPolling(self):
         print("doThingsboardPolling", time.ctime())
-        #r = self.session.get(self.gtfsrtFeedURL)
 
-        #if r.status_code != 200:
-            #print("GTFS RT feed returned with " + str(r.status_code))
-            #return
-        vehicles = [{ "latitude": 48, "longitude": 8, "id": "vehicle-1" }]
+        thingsboard_client = ThingsboardClient()
+        vehicles = thingsboard_client.get_vehicles()
 
         for vehicle in vehicles:
 
@@ -76,38 +105,17 @@ class GTFSRTHTTP2MQTTTransformer:
             nfeedmsg.header.gtfs_realtime_version = "1.0"
             nfeedmsg.header.incrementality = nfeedmsg.header.DIFFERENTIAL
             nfeedmsg.header.timestamp = int(time.time())
-            nent = nfeedmsg.entity.add()
-            nent.id = "entity-123"
+            ent = nfeedmsg.entity.add()
 
-            trip_id = "trip-123"
-            route_id = "route-01"
-            direction_id = 1
-            trip_headsign = "Herrenberg"
-            latitude = "{:.6f}".format(vehicle["latitude"]) # Force coordinates to have 6 numbers
-            latitude_head = latitude[:2]
-            longitude = "{:.6f}".format(vehicle["longitude"])
-            longitude_head = longitude[:2]
-            geohash_head = latitude_head + ";" + longitude_head
-            geohash_firstdeg = latitude[3] + "" + longitude[3]
-            geohash_seconddeg = latitude[4] + "" + longitude[4]
-            geohash_thirddeg = latitude[5] + "" + longitude[5]
-            stop_id = "stop-123"
-            start_time = "10:00" # hh:mm
-            vehicle_id = vehicle["id"]
-            short_name = "bus-1234"
-            color = "red"
+            ent.id = vehicle["id"]
+            ent.vehicle.position.latitude = vehicle['latitude']
+            ent.vehicle.position.longitude = vehicle['longitude']
 
-            # gtfsrt/vp/<feed_name>/<agency_id>/<agency_name>/<mode>/<route_id>/<direction_id>/<trip_headsign>/<trip_id>/<next_stop>/<start_time>/<vehicle_id>/<geohash_head>/<geohash_firstdeg>/<geohash_seconddeg>/<geohash_thirddeg>/<short_name>/<color>/
-            # GTFS RT feed used for testing was missing some information so those are empty
-            full_topic = '{0}/{1}////{2}/{3}/{4}/{5}/{6}/{7}/{8}/{9}/{10}/{11}/{12}/{13}/{14}/'.format(
-                self.baseMqttTopic, "vp", route_id, direction_id,
-                trip_headsign, trip_id, stop_id, start_time, vehicle_id, geohash_head, geohash_firstdeg,
-                geohash_seconddeg, geohash_thirddeg, short_name, color)
+            full_topic = f'{ self.baseMqttTopic }/vp/busses/{ vehicle["id"] }'
 
             print(full_topic)
 
             sernmesg = nfeedmsg.SerializeToString()
-            print(sernmesg)
             self.client.publish(full_topic, sernmesg)
 
 if __name__ == '__main__':
